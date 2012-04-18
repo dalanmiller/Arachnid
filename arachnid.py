fbb507408e3026753376309097758c1108386f84
Daniel Alan Miller <dalan.miller@gmail.com>
2012-04-17 22:11:51 -0700

    moar commits

--- arachnid.py
+++ arachnid.py
@@ -8,18 +8,50 @@ Arachnid will go through a given domain and find all resources that is hosted wi
 
 import os
 import requests
+import sys
+import urlparse 
+
+
+from BeautifulSoup import BeautifulSoup
+from requests import async
+
 import networkx as nx
 
 
 
-class resource(Model)
+class Resource():
 	def __init__(url, **kwargs):
 		self.url = url
 		for x,y in kwargs: 
 			self.__setattr__(x,y) 
 
+GLOBAL_WEB = {}
 
 def crawler(url):
-	pass
+
+
+	r = requests.get(url)
+
+	#Exits currently if the status code returns a 404. 
+	if r.status_code != 200:
+		print 'Need to handle other responses'
+		sys.exit()
+
+	#"Soups" the html
+	soup = BeautifulSoup(r.content)
+
+	#Uses BeautifulSoup to go through the page and grab all the hrefs if the tag has the attribute and the scheme is 'http'
+	page_links = [x['href'] for x in soup.findAll('a') if x.has_key('href') and urlparse(x['href']).scheme == 'http']
 	
+	for x in page_links:
+		if x not in GLOBAL_WEB.keys():
+
+			rs = [async.get(u) for u in page_links]
+
+			responses = async.map(rs)
+
+
+if __name__ == '__main__':
+	if sys.argv[1]: 
+		crawler(sys.argv[1])
 
