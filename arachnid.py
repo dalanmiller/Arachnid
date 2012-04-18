a73215580faa055d47836bb9fb2c0abcdd187cef
Daniel Alan Miller <dalan.miller@gmail.com>
2012-04-18 00:22:26 -0700

    Final commits before zzzzzZZZ time.

--- arachnid.py
+++ arachnid.py
@@ -1,7 +1,9 @@
 """
 Arachnid is a web crawler that is going to be awesome.
 
-Arachnid will go through a given domain and find all resources that is hosted within it. 
+Arachnid will go through a given domain and find all resources that is hosted within it and generate a network graph data structure. 
+
+Each function should be short, simple, and do one thing well.
 
 """
 
@@ -20,38 +22,100 @@ import networkx as nx
 
 
 class Resource():
-	def __init__(url, **kwargs):
+	"""
+	This class will (hopefully) handle a single URL resource aka a web page, image, css file, etc.
+	"""
+	def __init__(url, content, **kwargs):
 		self.url = url
+		self.content = content
 		for x,y in kwargs: 
 			self.__setattr__(x,y) 
 
-GLOBAL_WEB = {}
+class Web():
+	"""Main Web class that will handle just about everything that has to do with the crawled web"""
+	def __init__(self):
+		self.web = create_web()
 
-def crawler(url):
+	def create_web(): 
+		"""This function creates the networkx graph object
 
+			Put this into a function as I'm not sure if there are other variables we might want or options 
+			depending on certain situations
 
-	r = requests.get(url)
+		"""	
+		G = nx.Graph()
 
-	#Exits currently if the status code returns a 404. 
-	if r.status_code != 200:
-		print 'Need to handle other responses'
-		sys.exit()
+		return G
 
-	#"Soups" the html
-	soup = BeautifulSoup(r.content)
+	def create_node(resource_object):
+		"""This function will create a node where the node data is the resource object for that resource
 
-	#Uses BeautifulSoup to go through the page and grab all the hrefs if the tag has the attribute and the scheme is 'http'
-	page_links = [x['href'] for x in soup.findAll('a') if x.has_key('href') and urlparse(x['href']).scheme == 'http']
+			AKA : http://example.com/index.html 
+
+			self.web.create_node(Resource('http://example.com/index.html'))
+
+		"""
+		self.web.add_node(resource_object)
+		return True
+
+	def find_anchor_urls(raw_html):
+		"""
+		This function pulls the links out of anchor tags which have an 'href' attribute
+		"""
+		#"Soups" the html
+		soup = BeautifulSoup(raw_html)
+
+		#Uses BeautifulSoup to go through the page and grab all the hrefs if the tag has the attribute and the scheme is 'http'
+		#page_links = [x['href'] for x in soup.findAll('a') if x.has_key('href') and urlparse(x['href']).scheme == 'http']
+		page_links = []
+
+		#For each anchor tag found in the html
+		for x in soup.findAll('a'):
+			#If the tag has the attr 'href' and the scheme of the url is 'http'  
+			if x.has_key('href') and urlparse(x['href']).scheme == 'http':
+				#Append the url to the list
+				page_links.append(x['href'])
+
+		return page_links
+
+	def find_src_urls(raw_html):
+		"""
+		This function will go through link tags, img tags (and others?) to find tags which have the 'src' attribute
+		"""
+		return 
+
+	def crawler(url):
+		"""The main crawler function that will do the requests"""
+
+		r = requests.get(url)
+
+		#Need to check for redirect somewhere in here
+		page = Resource(r.url, r.content)
+
+		self.create_node(page)
+
+		#Exits currently if the status code returns a 404. 
+		if r.status_code != 200:
+			print 'Need to handle other responses'
+			sys.exit()
+
+		page_links = find_anchor_urls(page.content)
+
+		#This line creates a list of get requests objects (objects not yet sent)
+		rs = [async.get(u) for u in page_links]
+
+		#now we have a list of response objects, the data has gone and been collected
+		responses = async.map(rs)
+
 	
-	for x in page_links:
-		if x not in GLOBAL_WEB.keys():
-
-			rs = [async.get(u) for u in page_links]
-
-			responses = async.map(rs)
 
 
-if __name__ == '__main__':
+
+
+
+
+
+) __name__ == '__main__':
 	if sys.argv[1]: 
 		crawler(sys.argv[1])
 
--- readme.md
+++ readme.md
@@ -2,4 +2,26 @@
 
 _A new type of web crawler_
 
+
+###Problem
+The problem is that there are no good web crawlers out there. Well there might be but they all cost a lot of money and we think we can do better.
+There should be a service where people can crawl a website of unlimited amount of pages and get results in a network-esque format that 
+allows them to analyze the network and draw conclusions about that data to make their site more effective or get status information.
+Including but not limited to, page errors, broken links, rusting redirects, and much more. 
+
+###Requirements
+
+*	Able to scan and compile a networkx graph of a domain
+	-	Able to somewhat intelligently repair links within the domain to get a more complete picture (handles errors well?)
+	-	
+*	Able to output an image of all pages on the website with relevant data.
+	*Colors portray relevant data such as errors, redirects, links to external resources, etc.
+	*Maybe one day in a scannable google map interface?
+*	Data able to be put into a simple database
+*	Final data able to be viewable in a simple web interface
+	
+
+### Specifications
+
+*	
 Written by Greg Lamb and Daniel Miller
