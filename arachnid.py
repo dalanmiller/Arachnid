e3877e27e0e51f4cbce65788a19666d6f7869f40
Daniel Alan Miller <dalan.miller@gmail.com>
2012-04-24 01:06:47 -0700

    Max record on shop.nordstrom.com 1237 nodes

--- arachnid.py
+++ arachnid.py
@@ -13,11 +13,12 @@ import requests
 import sys
 
 from datetime import datetime
-from urlparse import urlparse
+from urlparse import urlparse, urljoin, urlunparse
 from BeautifulSoup import BeautifulSoup
 from requests import async
 
 import matplotlib.pyplot as plt
+import matplotlib
 import networkx as nx
 
 
@@ -30,6 +31,7 @@ class Resource(object):
 	def __init__(self, url, content, **kwargs):
 		self.url = url
 		self.content = content
+		print kwargs
 		for key in kwargs: 
 			self.__setattr__(key,kwargs[key]) 
 	def __repr__(self):
@@ -45,10 +47,14 @@ class Web(object):
 
 		#r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
+		print "Getting the content of the first node"
 		first_node_content = self.web.nodes()[0].content
 
+		print "Pulling the links from the first node content"
 		first_node_links = self.find_anchor_urls(first_node_content)
+		print first_node_links
 
+		print "Crawling the first links"
 		#To finalize the initialization process, run crawler function with each anchor tag link found on the first_url given.
 		map(self.crawler, first_node_links)
 
@@ -92,6 +98,9 @@ class Web(object):
 		"""
 		This function pulls the links out of anchor tags which have an 'href' attribute
 		"""
+		#Solves "expected string or buffer" error
+		if not raw_html: return []
+
 		#"Soups" the html
 		soup = BeautifulSoup(raw_html)
 
@@ -101,11 +110,34 @@ class Web(object):
 
 		#For each anchor tag found in the html
 		for x in soup.findAll('a'):
+			#Check if it has an href
+			if x.has_key('href') and not any(z in x['href'] for z in ['javascript', 'mailto']): 
 
-			#Check if it has an href, if the scheme is http, and then if the domain is the same as the first link used to init Web.
-			if x.has_key('href') and urlparse(x['href']).scheme == 'http' and urlparse(x['href']).netloc == urlparse(self.first_url).netloc:
-				#Append the url to the list
-				page_links.append(x['href'])
+				parsed_link = urlparse(x['href'])
+
+				if parsed_link.scheme == '' or parsed_link.netloc == '':
+
+					link = urljoin(self.first_url, x['href'])
+						
+					page_links.append(link)
+
+				elif parsed_link.netloc == urlparse(self.first_url).netloc:
+					link = x['href']
+
+					#Append the url to the list
+					page_links.append(link)
+
+		#Cleaning
+		for link in page_links:
+			
+			plink = urlparse(link)
+			
+			#Strip off the query string and fragment
+			link = urlunparse(plink[:]).replace("?"+plink[4], "")
+
+			#Strips the anchor fragment if it is found
+			if link.find('#') != -1:
+				link = link[:link.find('#')]
 
 		return page_links
 
@@ -122,7 +154,7 @@ class Web(object):
 		plt.axis('off')
 		plt.savefig("test_web"+str(datetime.time(datetime.now()))+".png")
 
-	def crawler(self,url, throttle = 5, url_limit = 1000):
+	def crawler(self,url, throttle = 5, url_limit = 100):
 		"""The main crawler function that will do the requests
 
 		Throttle: Value that limits number of concurrent requests that can be sent at any time
@@ -139,34 +171,50 @@ class Web(object):
 
 		self.create_node(page)
 
-		#Exits currently if the status code returns a 404. 
-		if r.status_code != 200:
-			print 'Need to handle other responses'
-			sys.exit()
-
 		page_links = self.find_anchor_urls(page.content)
 
-		#requests.defaults.defaults['pool_maxsize'] = 50
-		#requests.defaults.defaults['safe_mode'] = True
-		requests.defaults.defaults['max_retries'] = 1
+		#remove links already in the url_list from this list that is about to get crawled
+		page_links = [x for x in page_links if x not in self.url_list]
 
-		#This line creates a list of get requests objects (objects not yet sent)
-		rs = [async.get(u, config={'pool_maxsize':30}) for u in page_links]
+		requests.defaults.defaults['pool_maxsize'] = 50
+		requests.defaults.defaults['safe_mode'] = False
+		requests.defaults.defaults['max_retries'] = 0
 
+		try:
+			#This line creates a list of get requests objects (objects not yet sent)
+			rs = [async.get(u, prefetch=True) for u in page_links]
+
+		except:
+			#Attempts to recover from broken links within page_links
+			print "Trying to recover from broken links"
+			for p in page_links:
+				pp = urlparse(p)
+				if not all([p.scheme, p.netloc]):
+					page_links.pop(p)
+			print page_links
+			rs = [async.get(u) for u in page_links]
 		#now we have a list of response objects, the data has gone and been collected
 		responses = async.map(rs, size=throttle)
 
 		for resp in responses:
+			print "Here is the url | %s" %(resp.url)
+			print "And here is the list length | %s" % (self.url_list.__len__())
 			if resp.url not in self.url_list:
 				resource = Resource(resp.url, resp.__dict__)
+				print "Adding %s" % (resp.url)
 				self.url_list.append(resp.url)
-				print resource
 				self.create_node(resource)
 
-		if self.url_list.__len__() < url_limit: 
-			map(self.crawler, [r.url for r in responses])
-	
+		links = map(self.find_anchor_urls, [r.content for r in responses])
 
+		print "Crawl initiate! \n"
+		if self.url_list.__len__() < url_limit: 
+			try:
+				map(self.crawler, links)
+			except:
+				print "Error somewhere =("
+				for r in links:
+					self.crawler(r)
 
 
 
