f155d5da0e4fb4174cf5b9ed90a8f2ad2828716b
Daniel Alan Miller <dalan.miller@gmail.com>
2012-05-28 19:07:12 -0700

    Moved some methods out of the Web class as module functions, moved the text/html compile to the top of the file, switched from just using the Queue object for organization to now using rq, so the Queue contains rq.jobs which haven't or have completed.

--- arachnid2.py
+++ arachnid2.py
@@ -13,6 +13,84 @@ from BeautifulSoup import BeautifulSoup
 import matplotlib.pyplot as plt
 import networkx as nx
 
+# regex to just pull out the content type for html documents
+html_content = re.compile('text/html')
+
+def link_list_cleaner(link_list):
+    """
+    Ceaning link function
+    """
+    #Cleaning
+    for link in link_list:
+        
+        plink = urlparse(link)
+        
+        #Strip off the query string and fragment
+        link = urlunparse(plink[:]).replace("?"+plink[4], "")
+
+        #Strips the anchor fragment if it is found
+        if link.find('#') != -1:
+            link = link[:link.find('#')]
+
+    return link_list
+
+def url_getter(url, root_url):
+    """
+    Method used to send tasks to the queue
+    """
+    r = requests.get(url)
+
+    
+    if 'text/html' in r.headers['content-type']:
+        return find_anchor_urls(r.content, root_url = root_url)
+    else:
+        return []
+
+def find_anchor_urls(raw_html, root_url=None):
+    """
+    This function pulls the links out of anchor tags which have an 'href' attribute
+    """
+    #Solves "expected string or buffer" error
+    if not raw_html: 
+        return []
+
+    #"Soups" the html, try except statement in case html is badly formed
+    try:
+        soup = BeautifulSoup(raw_html)
+    except:
+        print "Shoddy html found"
+        return []
+
+    #Uses BeautifulSoup to go through the page and grab all 
+    #the hrefs if the tag has the attribute and the scheme is 'http'
+    #page_links = [x['href'] for x in soup.findAll('a') if 
+    #x.has_key('href') and urlparse(x['href']).scheme == 'http']
+    page_links = []
+    _base = urlparse(root_url).netloc
+    link = ''
+    for x in soup.findAll('a'):
+        #Check if it has an href
+        if x.has_key('href') and not any(z in x['href'] for z in ['javascript', 'mailto']): 
+
+            parsed_link = urlparse(x['href'])
+
+            if parsed_link.scheme == '' or parsed_link.netloc == '':
+
+                link = urljoin(root_url, x['href'])
+                    
+                page_links.append(link)
+
+            elif parsed_link.netloc == urlparse(root_url).netloc:
+                link = x['href']
+
+                #Append the url to the list
+                page_links.append(link)
+
+    #Remove query strings and anchor fragments
+    page_links = link_list_cleaner(page_links)     
+
+    return page_links
+
 class Web(object):
     """
     Our Web Object
@@ -21,73 +99,26 @@ class Web(object):
         self.first_url = url
         self.url_list = []
         self.web = nx.Graph()
-        self.queue =  Cue.Queue() 
+        self.q =  Queue() 
+        self.queue = Cue.Queue()
         self.crawler(url)
 
+
         while not self.queue.empty():
-            new_set_links = self.queue.get()
-            print "Now mapping through a new set of links"
-            map(self.crawler, new_set_links)
-       
-    def find_anchor_urls(self, raw_html):
-        """
-        This function pulls the links out of anchor tags which have an 'href' attribute
-        """
-        #Solves "expected string or buffer" error
-        if not raw_html: 
-            return []
+            new_job = self.queue.get()
+            if new_job.return_value == None:
+                print "Not yet!", new_job.id
+                self.queue.put(new_job)
+            else: 
+                print "Now mapping through a new set of links"
+                map(self.crawler, new_job.return_value)
 
-        #"Soups" the html, try except statement in case html is badly formed
-        try:
-            soup = BeautifulSoup(raw_html)
-        except:
-            print "Shoddy html found"
-            return []
+        self.draw_web()
 
-        #Uses BeautifulSoup to go through the page and grab all 
-        #the hrefs if the tag has the attribute and the scheme is 'http'
-        #page_links = [x['href'] for x in soup.findAll('a') if 
-        #x.has_key('href') and urlparse(x['href']).scheme == 'http']
-        page_links = []
-        _base = urlparse(self.first_url).netloc
-        link = ''
-        for x in soup.findAll('a'):
-            #Check if it has an href
-            if x.has_key('href') and not any(z in x['href'] for z in ['javascript', 'mailto']): 
-
-                parsed_link = urlparse(x['href'])
-
-                if parsed_link.scheme == '' or parsed_link.netloc == '':
-
-                    link = urljoin(self.first_url, x['href'])
-                        
-                    page_links.append(link)
-
-                elif parsed_link.netloc == urlparse(self.first_url).netloc:
-                    link = x['href']
-
-                    #Append the url to the list
-                    page_links.append(link)
-
-        #Remove query strings and anchor fragments
-        page_links = self.link_list_cleaner(page_links)     
-
-        return page_links
-
-    def link_list_cleaner(self, link_list):
-        #Cleaning
-        for link in link_list:
-            
-            plink = urlparse(link)
-            
-            #Strip off the query string and fragment
-            link = urlunparse(plink[:]).replace("?"+plink[4], "")
-
-            #Strips the anchor fragment if it is found
-            if link.find('#') != -1:
-                link = link[:link.find('#')]
-
-        return link_list
+        # while not self.queue.empty():
+        #     new_set_links = self.queue.get()
+        #     print "Now mapping through a new set of links"
+        #     map(self.crawler, new_set_links)
 
     def draw_web(self):
         """
@@ -111,42 +142,39 @@ class Web(object):
             self.web.add_node(url)
             self.web.node[url]['parent'] = 'True'
 
-        # regex to just pull out the content type for html documents
-        html_content = re.compile('text/html')
+        
         # create a request for the current page content
         r = requests.get(url)
         # Assign the the content type to a variable so we can use it,
         # as well as attaching it to the node
         content_type = r.headers['content-type']
-        self.web.node[url]['content-type'] = html_content.findall(content_type)[0]
+
+        #self.web.node[url]['content-type'] = html_content.findall(content_type)[0]
+
         self.web.node[url]['status-code'] = r.status_code
+
         self.web.node[url]['size'] = len(r.content)
+
         # Only scan text/html pages and assign the urls to a list
         if 'text/html' in html_content.findall(content_type):
-            page_links = self.find_anchor_urls(r.content)
+            page_links = find_anchor_urls(r.content, root_url = self.first_url)
 
-        for link in page_links:
-            if self.web.has_node(link):
-                self.web.add_edge(url, link)
-                page_links.remove(link)
-            else:
-                self.web.add_node(link, parent='False')
-                self.web.add_edge(url, link)
+            #Only want this loop to run anyway if the content is html? 
+            for link in page_links:
+                if self.web.has_node(link):
+                    self.web.add_edge(url, link)
+                    page_links.remove(link)
+                else:
+                    self.web.add_node(link, parent='False')
+                    self.web.add_edge(url, link)
 
-                # THIS IS WHERE WE WILL SEND EACH LINK TO THE QUEUE
-                print "Adding another link to the queue"
-                self.queue.put(self.url_getter(link))
+                    # THIS IS WHERE WE WILL SEND EACH LINK TO THE QUEUE
+                    print "Adding another link to the queue", link
+                    self.queue.put(self.q.enqueue(url_getter, link, self.first_url)) # put(self.url_getter(link))
 
-    def url_getter(self, url):
-        """
-        Method used to send tasks to the queue
-        """
-        r = requests.get(url)
-
-        return self.find_anchor_urls(r.content)
 
 if __name__ == '__main__':
-    #use_connection() #Connects to locally hosted Redis Server
+    use_connection() #Connects to locally hosted Redis Server
     if sys.argv[1]:
         test = Web(sys.argv[1])
         test.draw_web()
