e132b25a4907fcf47abc37ef9f3a46a33c1a5c44
Gregg Lamb <greggmlamb@gmail.com>
2012-06-05 19:02:47 -0700

    changing force.js

--- force/force.js
+++ force/force.js
@@ -1,45 +1,63 @@
-//Width and height
-var w = 500;
-var h = 100;
 
-var dataset = [
-            [5, 20], [480, 90], [250, 50], [100, 33], [330, 95],
-            [410, 12], [475, 44], [25, 67], [85, 21], [220, 88]
-           ];
+var w = 1024,
+    h = 1000,
+    fill=new Array()
+    fill[0] = 'blue';
+    fill[1] = 'red';
 
-//Create SVG element
-var svg = d3.select("body")
-         .append("svg")
-         .attr("width", w)
-         .attr("height", h);
+var vis = d3.select("#chart")
+  .append("svg:svg")
+    .attr("width", w)
+    .attr("height", h);
 
-svg.selectAll("circle")
-   .data(dataset)
-   .enter()
-   .append("circle")
-   .attr("cx", function(d) {
-         return d[0];
-   })
-   .attr("cy", function(d) {
-         return d[1];
-   })
-   .attr("r", function(d) {
-         return Math.sqrt(h - d[1]);
-   });
+d3.json("force.json", function(json) {
+  var force = d3.layout.force()
+      .charge(-120)
+      .linkDistance(100)
+      .nodes(json.nodes)
+      .links(json.links)
+      .size([w, h])
+      .start();
 
-svg.selectAll("text")
-   .data(dataset)
-   .enter()
-   .append("text")
-   .text(function(d) {
-        return d[0] + "," + d[1];
-   })
-   .attr("x", function(d) {
-        return d[0];
-   })
-   .attr("y", function(d) {
-        return d[1];
-   })
-   .attr("font-family", "sans-serif")
-   .attr("font-size", "11px")
-   .attr("fill", "red");
+  var link = vis.selectAll("line.link")
+      .data(json.links)
+    .enter().append("svg:line")
+      .attr("class", "link")
+      .style("stroke-width", 1)
+      .attr("x1", function(d) { return d.source.x; })
+      .attr("y1", function(d) { return d.source.y; })
+      .attr("x2", function(d) { return d.target.x; })
+      .attr("y2", function(d) { return d.target.y; });
+    
+
+  var node = vis.selectAll("circle.node")
+      .data(json.nodes)
+    .enter().append("svg:circle")
+      .attr("class", "node")
+      .attr("cx", function(d) { return d.x; })
+      .attr("cy", function(d) { return d.y; })
+      .attr("r", 5)
+      .style("fill", function(d) {
+        if (d.group == 0){
+          return "blue"
+        } })
+      // .call(force.drag);
+
+  node.append("svg:title")
+      .text(function(d) { return d.id; });
+
+  vis.style("opacity", 1e-6)
+    .transition()
+      .duration(1000)
+      .style("opacity", 1);
+
+  force.on("tick", function() {
+    link.attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
+
+    node.attr("cx", function(d) { return d.x; })
+        .attr("cy", function(d) { return d.y; });
+  });
+});
